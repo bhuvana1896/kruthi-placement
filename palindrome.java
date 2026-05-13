d78c7d5502b5c5cef176e31dc1acfac453ff4439
Bhuvana Kruthi <bhuvanakruthi1896@gmail.com>
2026-05-13 20:01:16 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+public class palindrome {
+
+    public static void main(String[] args) {
+
+        int n = 121;
+
+        int original = n;
+        int rev = 0;
+
+        while (n != 0) {
+
+            int digit = n % 10;
+
+            rev = rev * 10 + digit;
+
+            n = n / 10;
+        }
+
+        if (original == rev) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+    }
+}
