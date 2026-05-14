6fd855de54b7154ecc60746bd45b0a2a47d051a6
Bhuvana Kruthi <bhuvanakruthi1896@gmail.com>
2026-05-14 20:54:08 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString{
+    public static void main(String[]args){
+        String str = "kruthi";
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.println(str.charAt(i));
+
+        }
+    }
+}
