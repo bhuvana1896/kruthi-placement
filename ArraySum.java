f7292604022538e51f7f837726f34ab0d81cd2b8
Bhuvana Kruthi <bhuvanakruthi1896@gmail.com>
2026-05-14 21:20:42 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,14 @@
+public class ArraySum{
+    public static void main (String[]args){
+        int[] arr={100,150,200,250,300};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("SUM IS :"+sum);
+
+
+            
+        
+    }
+}
