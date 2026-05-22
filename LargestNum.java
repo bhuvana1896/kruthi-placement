587d1fc9a3c4f55f264cbcda43864f821a0024a3
Bhuvana Kruthi <bhuvanakruthi1896@gmail.com>
2026-05-22 18:53:53 +0530

    Add files via upload

---
+++ LargestNum.java
@@ -0,0 +1,14 @@
+public class LargestNum{
+    public static void main(String[]args){
+        int[] arr={1,5,3,7};
+        int largest=arr[0];
+        for(int i=1;i< arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+
+            }
+        }
+        System.out.println("LARGEST:" +largest);
+    }
+}
+          
